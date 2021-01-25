19ec239d7293b1187f8ab338a84876f6512b33ac
u4mc <mileschampion@hotmail.co.uk>
2021-01-25 19:57:08 +0000

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GearAllocator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
